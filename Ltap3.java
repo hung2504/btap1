7cb8608ff5a6f5acb157289e100c7213b4998eb0
hung2504 <43133441+hung2504@users.noreply.github.com>
2018-09-10 23:02:28 +0700

    Add files via upload

---
+++ Ltap3.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Ltap3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("canh cua hinh lap phuong la:");
+        int a = scanner.nextInt();
+        int v = a * a * a;
+        System.out.println("The tich hinh lap phuong la:"+v);
+
+
+
+    }
+}
